90fb0085694741e9bc3a6ef458b4d1f7a1fdda9b
Yinlin Chen <ylchen@vt.edu>
2019-10-18 09:56:13 -0400

    update readme

--- cypress/integration/location_spec.js
+++ cypress/integration/location_spec.js
@@ -6,12 +6,10 @@ context('Location', () => {
   })
 
   it('cy.hash() - get the current URL hash', () => {
-    // https://on.cypress.io/hash
     cy.hash().should('be.empty')
   })
 
   it('cy.url() - get the current URL', () => {
-    // https://on.cypress.io/url
     cy.url().should('eq', 'http://localhost:3000/')
   })
 })
