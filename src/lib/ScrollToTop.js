12f7aa06cd5b20f5e3b82778f3dd34882bd89dc7
Lee Hunter <whunter@vt.edu>
2020-04-01 16:27:44 -0400

    scrollTo page top on search change in addition to pathname change

--- src/lib/ScrollToTop.js
+++ src/lib/ScrollToTop.js
@@ -3,11 +3,14 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const search = window.location.search;
+  console.log(pathname);
+  console.log(search);
 
   useEffect(() => {
     const body = document.body;
     body.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, search]);
 
   return null;
 }
