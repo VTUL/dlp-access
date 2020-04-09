268f450118bc1769083903ccbbca2dd5f7c02f2d
Andrea Waldren <awaldren@vt.edu>
2020-04-09 09:17:56 -0400

    Added contact info, css, moved downloads link, and updated link on About page.

--- src/pages/PermissionsPage.js
+++ src/pages/PermissionsPage.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import SiteTitle from "../components/SiteTitle";
+import ContactSection from "../components/ContactSection";
 import { getHTML } from "../lib/fetch_tools";
 
+<<<<<<< HEAD:src/pages/PermissionsPage.js
 class PermissionsPage extends Component {
+=======
+import "../css/TermsPage.css";
+
+class TermsPage extends Component {
+>>>>>>> Added contact info, css, moved downloads link, and updated link on About:src/pages/TermsPage.js
   constructor(props) {
     super(props);
     this.state = {
@@ -17,6 +24,7 @@ class PermissionsPage extends Component {
   render() {
     return (
       <>
+<<<<<<< HEAD:src/pages/PermissionsPage.js
         <SiteTitle
           siteTitle={this.props.siteDetails.siteTitle}
           pageTitle="Permissions"
@@ -25,6 +33,34 @@ class PermissionsPage extends Component {
           className="terms-details"
           dangerouslySetInnerHTML={{ __html: this.state.copy }}
         ></div>
+=======
+        <div className="row terms-page-wrapper">
+          <div className="col-12 terms-heading">
+            <SiteTitle
+              siteTitle={this.props.siteDetails.siteTitle}
+              pageTitle="Terms"
+            />
+            <h1>Permissions</h1>
+          </div>
+          <div className="col-md-8">
+            <div
+              className="terms-details"
+              dangerouslySetInnerHTML={{ __html: this.state.copy }}
+            ></div>
+          </div>
+          <div className="col-md-4 contact-section-wrapper">
+            <ContactSection siteDetails={this.props.siteDetails} />
+            {this.props.siteDetails.termsCopy.download ? (
+              <div>
+                <p className="terms-downloads-heading">Downloadable forms</p>
+                <a href={this.props.siteDetails.termsCopy.download}>
+                  Permission form for image reproductions
+                </a>
+              </div>
+            ) : null}
+          </div>
+        </div>
+>>>>>>> Added contact info, css, moved downloads link, and updated link on About:src/pages/TermsPage.js
       </>
     );
   }
