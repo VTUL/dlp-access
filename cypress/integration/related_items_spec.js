fbc90f088c867a397ee8a3a2b292bfc95236a589
Andrea Waldren <awaldren@vt.edu>
2020-06-05 14:05:48 -0400

    Created Related items section

---
+++ cypress/integration/related_items_spec.js
@@ -0,0 +1,22 @@
+describe('Related items on archives page', () => {
+
+    it('Related items section shows on page', () => {
+        cy.visit("http://localhost:3000/archive/pf59ds4d");
+        cy.get('.related-items-wrapper')
+    })
+
+    it('Carousel populates when more than ten items in the item\'s subcollection', () => {
+        cy.get('.slick-slide')
+        .should('have.class', 'slick-active')
+    })
+
+    it("Carousel populates when less than 10 items in the item's subcollection, and more than 10 items in the parent's subcollections", () => {
+        cy.visit("http://localhost:3000/archive/p0636w4x");
+        cy.get(".slick-slide").should("have.class", "slick-active");
+    });
+
+    it('Carousel populates when less than 10 items in the item\'s subcollection, and less than 10 items in the parent\'s subcollections', () => {
+        cy.visit("http://localhost:3000/archive/ft77nv3b");
+        cy.get(".slick-slide").should('have.class', 'slick-active')
+    })
+})
