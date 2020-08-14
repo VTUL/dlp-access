6e2b020dd9ed7e66df65a9dfd5e36377570b6122
Andrea Waldren <awaldren@vt.edu>
2020-08-14 15:21:30 -0400

    Keyboard and SR navigation

--- cypress/integration/collection_metadata_display.spec.js
+++ cypress/integration/collection_metadata_display.spec.js
@@ -2,27 +2,27 @@ describe('A single Collection Show page metadata section', () => {
   beforeEach(() => {
     cy.visit('/collection/vb765t25');
     cy.get('#content-wrapper')
-      .find('div.details-section-content-grid')
+      .find('div.details-section-content-grid > table')
       .as('metadataSection')
   })
 
   it('displays the size field and its corresponding value', () => {
     cy.get('@metadataSection')
-      .find(':nth-child(1) > div.collection-detail-key')
+      .find(':nth-child(1) > th.collection-detail-key')
       .invoke('text')
       .should('equal', 'Size')
     cy.get('@metadataSection')
-      .find(':nth-child(1) > div.collection-detail-value')
+      .find(':nth-child(1) > td.collection-detail-value')
       .contains('Collections:')
   })
 
   it('displays the identifier field and its corresponding value', () => {
     cy.get('@metadataSection')
-      .find(':nth-child(7) > div.collection-detail-key')
+      .find(':nth-child(7) > th.collection-detail-key')
       .invoke('text')
       .should('equal', 'Identifier')
     cy.get('@metadataSection')
-      .find(':nth-child(7) > div.collection-detail-value').click()
+      .find(':nth-child(7) > td.collection-detail-value').click()
     cy.url().should('include', '/collection/vb765t25')
   })
 })
