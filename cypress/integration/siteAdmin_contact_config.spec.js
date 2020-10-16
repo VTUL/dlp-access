caf5078da9f8d4cdc9b3e5dcfb5f06a64758c6fa
Andrea Waldren <awaldren@vt.edu>
2020-10-16 13:51:48 -0400

    Added signout to cypress test

--- cypress/integration/siteAdmin_contact_config.spec.js
+++ cypress/integration/siteAdmin_contact_config.spec.js
@@ -4,7 +4,6 @@ const PASSWORD = Cypress.env("password");
 describe("Displays and updates contact configurations", () => {
   it("Logs in to admin site", () => {
     cy.visit("/siteAdmin");
-
     cy.get("amplify-authenticator")
       .find(selectors.usernameInput, {
         includeShadowDom: true,
@@ -24,25 +23,28 @@ describe("Displays and updates contact configurations", () => {
       .first()
       .find("button[type='submit']", { includeShadowDom: true })
       .click({ force: true });
-        
-    cy.wait(5000);
-  })
-      
-    it("Displays contact fields", () => {
-      cy.get("input[value='viewSite']").parent().click()
-      cy.contains("Contact 1")
-      cy.contains("Title: IAWA Archivist");
-      cy.contains("Contact 2")
-      cy.contains("Title: Board Chair")
-    });
-  
-  it("Updates contact fields", () => {
-    cy.get("input[value='editSite']").parent().click();
-    cy.get("#c0_title").clear().type("Director");
-    cy.contains("Update Site").click()
-    cy.contains("Title: Director");
   })
 
+  describe("Displays contact fields", () => {
+    it("Displays contact fields", () => {
+      cy.get("input[value='viewSite']")
+        .parent()
+        .click();
+      cy.contains("Contact 1").should("be.visible");
+      cy.contains("Title: IAWA Archivist").should("be.visible");
+      cy.contains("Contact 2").should("be.visible");
+      cy.contains("Title: Board Chair").should("be.visible");
+    });
+  });
+    
+  describe("Updates first contact's title and changes it back", () => {
+    it("Updates contact fields", () => {
+      cy.get("input[value='editSite']").parent().click();
+      cy.get("#c0_title").clear().type("Director");
+      cy.contains("Update Site").click()
+      cy.contains("Title: Director").should("be.visible");
+    })
+  
     it("Reverses update", () => {
       cy.get("input[value='editSite']")
         .parent()
@@ -51,9 +53,15 @@ describe("Displays and updates contact configurations", () => {
         .clear()
         .type("IAWA Archivist");
       cy.contains("Update Site").click();
-      cy.contains("Title: IAWA Archivist");
+      cy.contains("Title: IAWA Archivist").should("be.visible");
     });
+  });
 
+  after("User signout:", () => {
+  cy.get("amplify-sign-out")
+    .find(selectors.signOutButton, { includeShadowDom: true })
+    .contains("Sign Out").click({ force: true });
+  })
 });
 
 export const selectors = {
