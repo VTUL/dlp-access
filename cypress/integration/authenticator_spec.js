1cb0e314cc18badc8454c08fcfd5e8561a5f4d81
Lee Hunter <whunter@vt.edu>
2020-10-21 17:08:07 -0400

    refactor authenticator spec

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -8,24 +8,22 @@ describe("Authenticator:", function() {
  
     describe("Sign in and Sign out:", () => {
       it("allows a user to signin and signout", () => {
-        cy.get("amplify-authenticator")
+        cy.get("form")
           .find(selectors.usernameInput, {
             includeShadowDom: true,
           })
           .type(USERNAME);
 
-        cy.get("amplify-authenticator")
+        cy.get("form")
           .find(selectors.signInPasswordInput, {
             includeShadowDom: true,
           })
           .type(PASSWORD, { force: true });
 
-        cy.get("amplify-authenticator")
+        cy.get("form")
           .find(selectors.signInSignInButton, {
             includeShadowDom: true,
           })
-          .first()
-          .find("button[type='submit']", { includeShadowDom: true })
           .click({ force: true });
         
         cy.get("amplify-sign-out")
@@ -37,7 +35,7 @@ describe("Authenticator:", function() {
 
 export const selectors = {
   // Auth component classes
-  usernameInput: '[data-test="sign-in-username-input"]',
+  usernameInput: '[data-test="username-input"]',
   signInPasswordInput: '[data-test="sign-in-password-input"]',
   signInSignInButton: '[data-test="sign-in-sign-in-button"]',
   signOutButton: '[data-test="sign-out-button"]'
