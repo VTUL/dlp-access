b82d9400e4782488c20e2784ed699e399b1e9900
Lee Hunter <whunter@vt.edu>
2024-02-10 23:22:24 -0500

    imports and switch to my branch

--- src/components/MiradorViewer.js
+++ src/components/MiradorViewer.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Mirador from "mirador/dist/mirador.min.js";
+import Mirador from "mirador";
 import "../css/Viewer.scss";
 
 class MiradorViewer extends Component {
