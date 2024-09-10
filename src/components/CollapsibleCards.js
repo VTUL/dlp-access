2337c2f7c83e53a32884927e8c49995f5988d86f
Lee Hunter <whunter@vt.edu>
2024-09-10 11:41:10 -0400

    fix description bug, tiny tweaks

--- src/components/CollapsibleCards.js
+++ src/components/CollapsibleCards.js
@@ -1,19 +1,12 @@
-import * as React from "react";
+import { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import InfoIcon from "@mui/icons-material/Info";
 import CopyrightIcon from "@mui/icons-material/Copyright";
@@ -195,7 +188,7 @@ const getContent = (marker, data, site) => {
 };
 
 export default function CollapsibleCard({ title, marker, data, site }) {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
