8d3d149d101d0e3db37d7164898918b2bdfd3a4c
Lee Hunter <whunter@vt.edu>
2024-11-30 11:15:05 -0500

    babylon working with gltf/glb

--- src/components/BabylonElement.jsx
+++ src/components/BabylonElement.jsx
@@ -4,78 +4,72 @@ import * as BABYLON from "@babylonjs/core";
 import "../css/_3dViewer.scss";
 
 const BabylonElement = (props) => {
-  useEffect(() => {
-    registerBuiltInLoaders();
-    const canvasWrapper = document.getElementById("canvas-wrapper");
-    const canvas = createCanvas(canvasWrapper);
-    const engine = new BABYLON.Engine(canvas, true);
-    const createScene = () => {
-      const scene = new BABYLON.Scene(engine);
-      // const camera = new BABYLON.FreeCamera("camera1",
-      //     new BABYLON.Vector3(0, 5, -10), scene);
+  const disableScroll = () => {
+    [document.body, document.html].forEach((el) => {
+      el?.classList.add("no-scroll");
+    });
+  };
 
-      // The first parameter can be used to specify which mesh to import. Here we import all meshes
-      // const env = loadModel(scene, props.env);
-      const model = loadModel(scene, props.model);
-      const camera = new BABYLON.ArcRotateCamera(
-        "camera",
-        -Math.PI / 2,
-        Math.PI / 2.5,
-        4,
-        new BABYLON.Vector3(0, 0, 0),
-        scene
-      );
-      camera.setTarget(BABYLON.Vector3.Zero());
-      camera.attachControl(canvas, true);
-      camera.speed = 0.25;
-      //temporary light to light the entire scene
-      const light0 = new BABYLON.HemisphericLight(
-        "HemiLight",
-        new BABYLON.Vector3(0, 1, 0),
-        scene
-      );
-      // const light = new BABYLON.PointLight("sparklight", new BABYLON.Vector3(0, 0, 0), scene);
-      // light.diffuse = new BABYLON.Color3(0.08627450980392157, 0.10980392156862745, 0.15294117647058825);
-      // light.intensity = 35;
-      // light.radius = 1;
+  const enableScroll = () => {
+    [document.body, document.html].forEach((el) => {
+      el?.classList.remove("no-scroll");
+    });
+  };
 
-      return scene;
-    };
-    const scene = createScene();
+  const addListeners = (canvas, engine) => {
+    canvas.addEventListener("mouseover", disableScroll);
+    canvas.addEventListener("mouseout", enableScroll);
+
+    window.addEventListener("resize", () => {
+      engine.resize();
+    });
+  };
+
+  const removeListeners = (canvas, engine) => {
+    canvas.removeEventListener("mouseover", disableScroll);
+    canvas.removeEventListener("mouseout", enableScroll);
+
+    window.removeEventListener("resize", () => {
+      engine.resize();
+    });
+  };
+
+  const createScene = async (canvas, engine, modelURL) => {
+    const scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+    const model = await loadModel(scene, modelURL);
+    const modelDimensions = model.ellipsoid;
+    const modelMaxSize = Math.max(
+      modelDimensions._x,
+      modelDimensions._y,
+      modelDimensions._z
+    );
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      0,
+      0,
+      4,
+      new BABYLON.Vector3(0, 0, 0),
+      scene
+    );
+    camera.setPosition(new BABYLON.Vector3(0, 0, modelMaxSize * 2));
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.lowerRadiusLimit = modelMaxSize;
+    camera.upperRadiusLimit = modelMaxSize * 10;
+    camera.attachControl(canvas, true);
+    camera.speed = 0.1;
+    camera.minZ = 0.1;
+    //temporary light to light the entire scene
+    const light0 = new BABYLON.HemisphericLight(
+      "HemiLight",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
 
     engine.runRenderLoop(function () {
       scene.render();
     });
-
-    const disableScroll = () => {
-      [document.body, document.html].forEach((el) => {
-        el?.classList.add("no-scroll");
-      });
-    };
-
-    const enableScroll = () => {
-      [document.body, document.html].forEach((el) => {
-        el?.classList.remove("no-scroll");
-      });
-    };
-
-    canvas.addEventListener("mouseover", function () {
-      disableScroll();
-    });
-
-    canvas.addEventListener("mouseout", function () {
-      enableScroll();
-    });
-
-    window.addEventListener("resize", function () {
-      engine.resize();
-    });
-
-    return () => {
-      engine.dispose();
-      enableScroll();
-    };
-  }, [props.model]);
+  };
 
   const createCanvas = (canvasWrapper) => {
     const canvas = document.createElement("canvas");
@@ -98,10 +92,26 @@ const BabylonElement = (props) => {
       scene
     );
     const model = response.meshes[0];
-    const allMeshes = model.getChildMeshes();
 
-    return { model, allMeshes };
+    return model;
   };
+
+  useEffect(() => {
+    registerBuiltInLoaders();
+    const canvasWrapper = document.getElementById("canvas-wrapper");
+    const canvas = createCanvas(canvasWrapper);
+    const engine = new BABYLON.Engine(canvas, true);
+
+    createScene(canvas, engine, props.model);
+
+    addListeners(canvas, engine);
+
+    return () => {
+      removeListeners(canvas, engine);
+      enableScroll();
+      engine.dispose();
+    };
+  }, [props.model]);
 
   return (
     <section style={{ width: "100%", height: "100%" }}>
